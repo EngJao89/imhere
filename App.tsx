6a8a1ffdfc19e8eccfeed8ce2c5849c5c6793ac5
EngJao89 <eng_joao@outlook.com>
2023-01-28 22:25:20 -0300

    feat(02-jsx): treina conceitos de JSX com componentes base

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>João Ricardo! Arquiteto Mobile.</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+  <View>
+    <Text>Arquiteto de Software</Text>
+    <Text>Mobile</Text>
+  </View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
