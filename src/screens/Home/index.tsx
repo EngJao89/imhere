34bc8f840dfdf848b27f0bb9cd3a32de1a486bd4
EngJao89 <eng_joao@outlook.com>
2023-01-31 21:50:12 -0300

    feat(08-button-add): implementa botão de adicionar dados ao app

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,15 @@
-import { Text, View, TextInput } from 'react-native';
+import { 
+  Text, 
+  View, 
+  TextInput, 
+  TouchableOpacity 
+} from 'react-native';
 import { styles } from './styles';
 
 export function Home(){
+  function handleParticipantAdd(){ 
+    console.log("Você clicou no botão de Adicionar!");
+  }
   return(
   <View style={styles.container}>
     <Text style={styles.eventName}>
@@ -17,6 +25,15 @@ export function Home(){
       placeholder="Nome do participante"
       placeholderTextColor="#6B6B6B"
     />
+
+    <TouchableOpacity 
+      style={styles.button}
+      onPress={handleParticipantAdd}
+    >
+      <Text style={styles.buttonText}>
+        +
+      </Text>
+    </TouchableOpacity>
   </View>
   )
 }
